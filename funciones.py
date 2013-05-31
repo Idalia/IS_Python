2847c64d93ee9cb088ccd17e5735d163d9ea65eb
Idalia <idalia_0107@hotmail.com>
2013-05-31 13:02:19 -0600

    Tarea 2 de python
    Las operaciones de multiplicacion y division

---
+++ funciones.py
@@ -0,0 +1,22 @@
+from lib.operaciones import suma
+from lib.operaciones import HttpRequestResponeRedirect as RE
+
+valor = int(raw_input('ingresa un valor'))
+valor2 = int(raw_input('ingresa un valor'))
+operacion = raw_input('¿Que operaciones desea realizar : {s - r - m - d}')
+
+if operacion.lower == 's':
+    print suma(valor,valor2)
+
+elif operacion.lower == 'r':
+    print resta(valor,valor2)
+    
+elif operacion.lower == 'm':
+    print multiplicacion(valor,valor2)
+
+elif operacion.lower == 'd':
+    print division(valor,valor2)
+    
+
+
+print RE
